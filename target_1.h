205280348f97f460111c32aa596cdf13cee1173c
chessmaster987 <83759947+chessmaster987@users.noreply.github.com>
2021-05-26 22:42:55 +0300

    Add files via upload

---
+++ target_1.h
@@ -0,0 +1,24 @@
+#ifndef TARGET_H
+#define TARGET_H
+
+
+class Target
+{
+public:
+    Target();
+    virtual ~Target();
+
+    unsigned int Getm_Counter() { return m_Counter; }
+    void Setm_Counter(unsigned int val) { m_Counter = val; }
+
+    int GetShot() const { return m_Shot; }
+    /** Set m_Shot
+     * \param val New value to set
+     */
+ 
+
+private:
+    unsigned int m_Counter;
+};
+
+#endif // TARGET_H
