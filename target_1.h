0e2cb5d70c91749a080cf117267a6aaf18db1836
Vlad Lavrov <83759947+chessmaster987@users.noreply.github.com>
2021-06-01 22:10:33 +0300

    Update target_1.h

--- target_1.h
+++ target_1.h
@@ -1,22 +1,16 @@
 #ifndef TARGET_H
 #define TARGET_H
 
-
-class Target
-{
+class Target{
 public:
     Target();
     virtual ~Target();
-
     unsigned int Getm_Counter() { return m_Counter; }
     void Setm_Counter(unsigned int val) { m_Counter = val; }
-
     int GetShot() const { return m_Shot; }
     /** Set m_Shot
      * \param val New value to set
      */
- 
-
 private:
     unsigned int m_Counter;
 };
