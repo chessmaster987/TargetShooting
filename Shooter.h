da03920b2de41607abb0b809d2ff4ebf2584b143
Vlad Lavrov <83759947+chessmaster987@users.noreply.github.com>
2021-06-10 19:13:17 +0300

    Update Shooter.h

--- Shooter.h
+++ Shooter.h
@@ -6,5 +6,19 @@ class Shooter
 {
 public:
 	Shooter(string name);
+
+	// геттери
+	string getName() const;
+	size_t getShotsRate() const;
+	// сеттери
+	void setName(string name);
+	void setShotsRate(size_t shotsRate);
+
+	void addShotsRate(size_t rate);
+
+
 private:
+	string name;
+	size_t shotsRate;
 };
+
