51224355561b3f4f27fa96896d19307f954f53d6
Vlad Lavrov <83759947+chessmaster987@users.noreply.github.com>
2021-06-10 19:12:33 +0300

    Update Shooter.cpp

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,1 +1,36 @@
-#include <iostream>
+#include "Shooter.h"
+Shooter::Shooter(string name)
+{
+	setName(name);
+	setShotsRate(0);
+}
+
+string Shooter::getName() const
+{
+	return name;
+}
+
+size_t Shooter::getShotsRate() const
+{
+	return shotsRate;
+}
+
+void Shooter::setName(string name)
+{
+	if (name.empty())
+		this->name = "no name";
+	else
+		this->name = name;
+}
+
+void Shooter::setShotsRate(size_t shotsRate)
+{
+	this->shotsRate = shotsRate;
+}
+
+void Shooter::addShotsRate(size_t rate)
+{
+	shotsRate += rate;
+}
+
+
